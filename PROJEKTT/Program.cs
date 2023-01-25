681afa30484de8a215a6672061f8c2262825a625
Filip <Filip@DESKTOP-B0PLODT>
2023-01-26 00:07:17 +0100

    Dodaj pliki projektów.

---
+++ PROJEKTT/Program.cs
@@ -0,0 +1,32 @@
+﻿ class Program
+    //program zawiera
+    //dictionary
+    //try-catch-finally
+    {
+        static void Main(string[] args)
+        {
+
+            // Tworzymy słownik z kluczami i wartościami
+            Dictionary<string, int> slownik = new Dictionary<string, int>();
+
+            // Dodajemy elementy do słownika
+            slownik.Add("Jeden", 1);
+            slownik.Add("Trzy", 3);
+            slownik.Add("Pięć", 5);
+
+            try // Próba wypisania liczby "Sześć", której nie ma w słowniku
+            {
+                Console.WriteLine(slownik["Sześć"]);
+
+            }
+            catch (KeyNotFoundException) // Wynik po złapaniu wyjątku
+            {
+                Console.WriteLine("Wartość nie istnieje w słowniku");
+            }
+            finally // Wynik końcowy, bez znaczenia czy wystąpił wyjątek
+            {
+                Console.WriteLine("Końcowa wiadomość :)");
+            }
+        }
+    }
+
